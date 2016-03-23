c890d1c96e173121197c85279ebdf853467a7945
Celeste Lavoie <celeste.lavoie@gmail.com>
2016-03-23 19:08:26 -0400

    SPACING Y U DO THIS TO ME

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ var $ball = $('.ball');
 var $picker = $('form');
 var $input = $('#pick-color');
 
-
 $picker.on('change', function () {
   console.log('works');
   $ball.css('background-color', $input.val());
